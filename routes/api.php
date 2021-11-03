a18d4aa535cfbc07beefa122ec46c2aaf4e7a9d9
Leo franca <leoprofissional@live.com>
2021-11-03 08:33:36 -0300

    feat: cira crud user

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::apiResource('/users', UserController::class);
 
 Route::get('/', function () {
     return response()->json(['message' => 'ok']);
